63c197c19b609c1130708d1b4a63f87fe19d8239
seb <s.delclos@skara.ovh>
2021-05-05 22:49:19 +0200

    divers fix dont dashboard

--- app/src/main/java/com/example/app_stallman/Dashboard.java
+++ app/src/main/java/com/example/app_stallman/Dashboard.java
@@ -3,17 +3,49 @@ package com.example.app_stallman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Dashboard extends AppCompatActivity {
+    private TextView openTextDsh;
+    private Button goMessgaeDsh;
+    private Button goBillDsh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        this.openTextDsh = (TextView) findViewById(R.id.titleDashboard);
+        this.goMessgaeDsh = (Button) findViewById(R.id.DshMessageListButton);
+        this.goBillDsh = (Button) findViewById(R.id.DshBillListButton);
+
+        SharedPreferences prefs = getApplicationContext().getSharedPreferences("connected_user", MODE_PRIVATE);
+        String userName = prefs.getString("nom_user","toto");
+        String userSurname = prefs.getString("prenom_user","tutu");
+
+        openTextDsh.setText("Bonjour " + userName + " " + userSurname );
+
+        goBillDsh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goBill();
+            }
+        });
+
+        goMessgaeDsh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goMessage();
+            }
+        });
+
     }
 
     private void goMessage() {
